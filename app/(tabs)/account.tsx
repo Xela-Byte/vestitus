ab15a3ad1ba29b29b9789170cc1658bfda941748
Xela <xelaoladipupo@gmail.com>
2025-12-12 12:10:48 +0100

    feat: Enhance AccountScreen layout with additional sections and icons for better user navigation

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,4 +1,9 @@
+import AppText from "@/components/ui/AppText";
 import HeaderComponent from "@/components/ui/HeaderComponent";
+import { FontAwesome5 } from "@expo/vector-icons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -6,12 +11,87 @@ type Props = Record<string, never>;
 
 const AccountScreen = (props: Props) => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <View className="bg-stroke">
+      <SafeAreaView className="bg-white" />
       {/* Header */}
-      <View className="px-5">
-        <HeaderComponent title="Account" />
+      <View className="px-5 bg-white">
+        <View className="border-b border-b-stroke">
+          <HeaderComponent title="Account" />
+        </View>
       </View>
-    </SafeAreaView>
+
+      <View className="px-5 flex flex-row items-center justify-between bg-white">
+        <View className="py-7 flex flex-row items-center gap-x-3">
+          <Feather name="box" size={24} color="black" />
+          <AppText>My Orders</AppText>
+        </View>
+
+        <Feather name="chevron-right" size={24} color="#808080" />
+      </View>
+
+      <View className="my-3">
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <FontAwesome name="address-card-o" size={20} color="black" />
+            <AppText>My Details</AppText>
+          </View>
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <Feather name="home" size={24} color="black" />
+            <AppText>My Address</AppText>
+          </View>
+
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <FontAwesome name="credit-card" size={22} color="black" />
+            <AppText>Payment Methods</AppText>
+          </View>
+
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <Feather name="bell" size={22} color="black" />
+            <AppText>Notifications</AppText>
+          </View>
+
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+      </View>
+
+      <View className="mb-3">
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <AntDesign name="question-circle" size={20} color="black" />
+            <AppText>FAQs</AppText>
+          </View>
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <FontAwesome5 name="headset" size={20} color="black" />
+            <AppText>Customer Support</AppText>
+          </View>
+
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+      </View>
+
+      <View className="h-1/2 bg-white">
+        <View className="px-5 flex flex-row items-center justify-between bg-white">
+          <View className="py-5 flex flex-row items-center gap-x-3">
+            <Feather name="log-out" size={22} color="red" />
+            <AppText className="text-red-500">Logout</AppText>
+          </View>
+
+          <Feather name="chevron-right" size={24} color="#808080" />
+        </View>
+      </View>
+    </View>
   );
 };
 
